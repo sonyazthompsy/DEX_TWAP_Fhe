d0d87c31f513cb32cc56af52936a9702278cea16
sonyazthompsy <eruslans731949381@rambler.ru>
2025-12-05 10:11:39 +0800

    add index.ts, queries.java, vercel.json

---
+++ base/queries.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\base\queries.java
+// Generated at: 2025-10-25T06:31:23.700Z
---
+++ frontend/web/src/service-message-pages.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\service-message-pages.go
+// Generated at: 2025-10-25T06:31:23.714Z
